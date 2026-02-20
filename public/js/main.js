e6323fec640c0898f1fddd9e20aa76337f5ce4b2
Oliver <oliver@wcls.nl>
2026-02-20 21:28:33 +0100

    auto-claude: subtask-2-3 - Integrate audio module with network and main game loop

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,7 @@ import Game from './game.js';
 import Input from './input.js';
 import Network from './network.js';
 import Renderer from './renderer.js';
+import Audio from './audio.js';
 
 var nameOverlay = null;
 var nameInput = null;
@@ -26,6 +27,7 @@ function init() {
 
   Renderer.init(document.body);
   Input.init();
+  Audio.init();
   Network.connect();
 
   nameSubmit.addEventListener('click', handleNameSubmit);
